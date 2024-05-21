2955f08eeee2ecf30690a690c38266782482c060
Tia Nguyen <trang@live.de>
2024-05-21 12:54:07 +0200

    tourio

---
+++ next.config.js
@@ -0,0 +1,37 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "www.visitberlin.de",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "www.google.de",
+        port: "",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
